93bd0b0b3076232efb5448946af1ec81f95ea75e
alvaro.navarro <alvaro.navarro@manifiesto.biz>
2023-10-02 18:05:51 +0200

    Adding form styles

--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <form>
+    <form class="presupuesto">
 
         <div class="campo">
             <label for="nuevo-presupuesto">Definir Presupuesto</label>
@@ -10,6 +10,7 @@
             <input id="nuevo-presupuesto"
                    class="nuevo-presupuesto"
                    placeholder="Añade tu presupuesto" 
+                   type="number"
             />
         </div>
 
@@ -18,5 +19,47 @@
 </template>
 
 <style scoped>
+    .presupuesto {
+        width: 100%;
 
+    }
+
+    .campo {
+        display: grid;
+        gap: 2rem;
+    }
+
+    .presupuesto label {
+        font-size: 2.2rem;
+        font-weight: 800;
+        text-align: center;
+        color: var(--blue);
+    }
+
+    .presupuesto input[type="number"] {
+        background-color: var(--light-grey) ;
+        border-radius: 1rem;
+        padding: 1rem;
+        border: none;
+        font-size: 2.2rem;
+        text-align: center;
+    }
+
+    .presupuesto input[type="submit"] {
+        background-color: var(--blue);
+        border: none;
+        padding: 1rem;
+        text-align: center;
+        font-size: 2rem;
+        margin-top: 2rem;
+        color: var(--white);
+        font-weight: 900;
+        width: 100%;
+        transition: background-color ease 300ms;
+    }
+
+    .presupuesto input[type="submit"]:hover {
+        cursor: pointer;
+        background-color: #1048A4;
+    }
 </style>
