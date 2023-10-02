04f1e0d821717d3192b103d1ce4cd58168c422d7
alvaro.navarro <alvaro.navarro@manifiesto.biz>
2023-10-02 17:33:30 +0200

    Adding global CSS && SASS installation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,61 @@
 <script setup>
+  
 
 </script>
 
 <template>
-  
+  <div>
+    <header>
+      <h1>Planificador de Gastos</h1>
+    </header>
+
+  </div>
 </template>
 
-<style scoped>
+<style>
+  :root {
+    --blue: #3b82f6;
+    --white: #FFF;
+    --light-grey: #F5F5F5;
+    --grey: #94a3b8;
+    --dark-gray: #64748b;
+    --dark: #000;
+  }
+
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: "Lato", sans-serif;
+    background-color: var(--light-grey);
+  }
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  header {
+    background-color: var(--blue);
+  }
+
+  header h1 {
+    padding: 3rem 0;
+    margin: 0;
+    color: var(--white);
+    text-align: center;
+  }
 
 </style>
---
+++ src/components/Filtro.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h2>Filtros</h2>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
